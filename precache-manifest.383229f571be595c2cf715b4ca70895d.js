af0caaf1972f61f969b4830eaa30c45678f6f3ef
Ifeoma <ifeomaidoko@gmail.com>
2021-03-10 13:15:58 -0700

    Updates

---
+++ precache-manifest.383229f571be595c2cf715b4ca70895d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5471c464b4be49f75e704c9cc2a64eb",
+    "url": "/google_keep_clone/index.html"
+  },
+  {
+    "revision": "205562d85bed0982d688",
+    "url": "/google_keep_clone/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "856dc9b29facb9d4e994",
+    "url": "/google_keep_clone/static/css/main.0f9b5a66.chunk.css"
+  },
+  {
+    "revision": "205562d85bed0982d688",
+    "url": "/google_keep_clone/static/js/2.1308f716.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/google_keep_clone/static/js/2.1308f716.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "856dc9b29facb9d4e994",
+    "url": "/google_keep_clone/static/js/main.556a3f16.chunk.js"
+  },
+  {
+    "revision": "957f166d3f5d01978fc8",
+    "url": "/google_keep_clone/static/js/runtime-main.69d361c7.js"
+  }
+]);
